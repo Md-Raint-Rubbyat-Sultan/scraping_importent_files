dffb35d56b2dea22814f5101aca6143f2c63a622
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2024-04-29 10:31:08 +0600

    user-agent findder file is attatched

---
+++ user_agent.py
@@ -0,0 +1,5 @@
+# use the following link/url to find user agent of your own pc/mac. This doesn't give any fake user agent.
+# usage: just copy the url - https://httpbin.org/get and past it into your browser.
+# it will return an object. find the user-agent form the object.
+
+'''Find Your User-Agent: https://httpbin.org/get'''
